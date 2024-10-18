778ef535deb7d56ded6dff8501fc91f8312d26a5
YASER-22 <3yasr51499@gmail.com>
2024-10-17 21:25:08 -0800

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.W
+{
+    public class student
+    {
+        private string name;
+        private string address;
+        private double mark;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+
+        }
+        public double Mark
+        {
+            get { return mark; }
+            set { mark = value; }
+
+        }
+        static void Main(string[] args)
+        {
+            student Y = new student();
+            Y.Name = "YASER";
+            Y.Address = "iraq";
+            Y.Mark = 75.14;
+            Console.WriteLine("The name is : " + Y.Name);
+            Console.WriteLine("frome : " + Y.Address);
+            Console.WriteLine("avareg : " + Y.Mark);
+            Console.ReadKey();
+        }
+    }
+}
